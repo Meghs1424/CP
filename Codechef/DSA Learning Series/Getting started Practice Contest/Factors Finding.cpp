2458c66409124588496abfa3e21954163776d68e
Meghs1424 <66918575+Meghs1424@users.noreply.github.com>
2021-09-28 13:07:30 +0530

    Create Factors Finding.cpp

---
+++ Codechef/DSA Learning Series/Getting started Practice Contest/Factors Finding.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, i = 1, count = 0;
+    vector<int> fact;
+    cin>>n;
+    while(i<=n) {
+        if(n%i == 0){
+            count++;
+            fact.push_back(i);
+        }
+        i++;
+    }
+    cout<<count<<'\n';
+    for(int j=0; j<fact.size(); j++) {
+        cout<<fact[j]<<' ';
+    }
+}

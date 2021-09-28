989dbca5ec0e426057d4c43399e4d657ad814049
Meghs1424 <66918575+Meghs1424@users.noreply.github.com>
2021-09-28 13:14:30 +0530

    Create Find Second Largest.cpp

---
+++ Codechef/DSA Learning Series/Getting started Practice Contest/Find Second Largest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin>>a>>b>>c;
+    if(a >= b && a >= c) {
+        if(b>=c)
+            cout<<b;
+        else
+            cout<<c;
+    }
+    else if(b >= a && b>= c) {
+        if(a>=c)
+            cout<<a;
+        else
+            cout<<c;
+    }
+    else {
+        if(b>=a)
+            cout<<b;
+        else
+            cout<<a;
+    }
+    return 0;
+}
